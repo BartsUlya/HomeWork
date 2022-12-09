edba613fe4a7ce3048b31336022d2caa82dbcaa1
BartsUlya <118444076+BartsUlya@users.noreply.github.com>
2022-12-09 17:16:17 +0400

    Task 10

---
+++ Task10.cs
@@ -0,0 +1,11 @@
+Console.WriteLine("Введите трехзначное числа");
+int number = int.Parse(Console.ReadLine()!);
+if (number < 0)
+{
+    number = -number;
+}
+if (100 <= number && number <= 999)
+{
+    int secondDigit = number / 10 % 10;
+    Console.WriteLine(secondDigit);
+}
