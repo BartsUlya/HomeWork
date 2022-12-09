94332c72334ded23e44e079ddf1d6d9f0ba2a0cf
BartsUlya <118444076+BartsUlya@users.noreply.github.com>
2022-12-09 17:27:58 +0400

    Task15

---
+++ task15.cs
@@ -0,0 +1,31 @@
+Console.WriteLine("Введите число ");
+string input1 = Console.ReadLine()!;
+int digit = int.Parse(input1);
+if (digit == 1)
+{
+    Console.WriteLine("нет");
+}
+else if (digit == 2)
+{
+    Console.WriteLine("нет");
+}
+else if (digit == 3)
+{
+    Console.WriteLine("нет");
+}
+else if (digit == 4)
+{
+    Console.WriteLine("нет");
+}
+else if (digit == 5)
+{
+    Console.WriteLine("нет");
+}
+else if (digit == 6)
+{
+    Console.WriteLine("да");
+}
+else if (digit == 7)
+{
+    Console.WriteLine("да");
+}
